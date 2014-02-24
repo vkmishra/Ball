7f8bb86dc4de1ba044a39c2fc49716d127e2f000
Vishwakant Mishra <vkmishra2050@gmail.com>
2014-02-25 04:36:37 +0530

    First Commit

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+		offset.y = offset.y - 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+	public float speed;
+	public GUIText scoreText, finish;
+	private int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText.text = "";
+		finish.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float movehorizontal = Input.GetAxis ("Horizontal");
+		float movevertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (movehorizontal, 0, movevertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+		scoreText.text = "Score: " + score.ToString ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "bcollectible") {
+						score += 7;
+						other.gameObject.SetActive (false);
+				} else if (other.tag == "scollectible") {
+						score += 4;
+						other.gameObject.SetActive (false);
+				} else if (other.tag == "door") {
+						finish.text="Congrats!! You Win!! Your score: "+score.ToString();
+				}
+	}
+}
